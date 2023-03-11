dcf546e95e1265801ea183d01417309a9a791ebe
johnsonakpama <johnsonakpama33@gmail.com>
2023-03-11 15:58:38 +0000

    still on assignment

---
+++ styles/script/togglemenu.js
@@ -0,0 +1,13 @@
+function toggleMenu () {
+    document.getElementById("primary-nav").classList.toggle("open");
+    document.getElementById("hamburger-button").classList.toggle("open");
+}
+
+const x = document.getElementById('hamburger-button')
+x.onclick = toggleMenu;function toggleMenu () {
+    document.getElementById("primary-nav").classList.toggle("open");
+    document.getElementById("hamburger-button").classList.toggle("open");
+}
+
+const x = document.getElementById('hamburger-button')
+x.onclick = toggleMenu;
